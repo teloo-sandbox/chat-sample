7d7ab5e578d829248a66c78563f0131722938f9f
Teloo <teru1721@gmail.com>
2014-04-19 23:08:59 +0900

    メッセージの取得・送信部分をサービスに抽出

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,37 @@
 angular.module('chatSampleModule', [])
-  .controller('MessageListController', function($scope, $http) {
-    $http
-      .get('api/messages')
-      .success(function(data, status, headers, config) {
-        $scope.messages = data;
-      })
-      .error(function(data, status, headers, config) {
-        throw new Error('メッセージリストの取得に失敗しました');
-      });
+  .factory("MessageManager", function($http) {
+    return {
+      getMessages: function(callback) {
+        $http
+          .get('api/messages')
+          .success(function(data, status, headers, config) {
+            callback(null, data);
+          })
+          .error(function(data, status, headers, config) {
+            callback(new Error('メッセージリストの取得に失敗しました'));
+          });
+      },
+      addMessage: function(message, callback) {
+        $http
+          .post('api/messages', message)
+          .success(function(data, status, headers, config) {
+            callback(null);
+          })
+          .error(function(data, status, headers, config) {
+            callback(new Error('メッセージの送信に失敗しました'));
+          });
+      }
+    };
   })
-  .controller('MessageEditorController', function($scope, $http) {
+  .controller('MessageListController', function($scope, $http, MessageManager) {
+    MessageManager.getMessages(function(err, messages) {
+      if (err) {
+        throw err;
+      }
+      $scope.messages = messages;
+    });
+  })
+  .controller('MessageEditorController', function($scope, $http, MessageManager) {
     var initMessage = function() {
       return {
         username: '',
@@ -20,13 +42,11 @@ angular.module('chatSampleModule', [])
     $scope.newMessage = initMessage();
 
     $scope.addMessage = function() {
-      $http
-        .post('api/messages', $scope.newMessage)
-        .success(function(data, status, headers, config) {
-          $scope.newMessage = initMessage();
-        })
-        .error(function(data, status, headers, config) {
-          throw new Error('メッセージの送信に失敗しました');
-        });
+      MessageManager.addMessage($scope.newMessage, function(err) {
+        if (err) {
+          throw err;
+        }
+        $scope.newMessage = initMessage();
+      });
     };
   });
